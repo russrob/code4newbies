7759ab142d210326e3d7aa4f6e69c26ae079eb0f
Russell Roberts <russrob23@gmail.com>
2016-09-18 16:45:48 -0700

    added bootstrap gem and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  root "pages#home"
+
+  get "pages/about"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
